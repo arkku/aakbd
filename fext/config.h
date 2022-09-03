c0d4db68b3e957ab14475a9901b318438b9f92b7
Kimmo Kulovesi <kk@arkku.com>
2022-09-03 17:39:00 +0300

    Correct FEXT hardcoded threshold

--- fext/config.h
+++ fext/config.h
@@ -44,7 +44,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define CAPSENSE_CAL_AUTOSAVE 0
 #endif
 
-#define CAPSENSE_HARDCODED_THRESHOLD 142
+#define CAPSENSE_HARDCODED_THRESHOLD 568
 
 #define CAPSENSE_KEYMAP_COL_TO_PHYSICAL_COL(col) (15-(col))
 
